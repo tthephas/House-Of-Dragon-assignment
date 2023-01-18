72d57dd3dd7ca87c48eab87befce08eebc81eba8
thepjos <josh.thephasdin@gmail.com>
2023-01-18 08:49:21 -0600

    added bio frame

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log('do something')
+
+// Need to create a few things
+// A pop up message for each Bio for each player
+// A pop up message for each status of each player (alive or dead)
+
+// The pop ups will use JS for document listeners. When clicked, the pop up will display. Need to put a CLOSE button on each too, or try to just close on clicking on something else. Or a timer.
+// Each one will probably need their own ID.
+
+// Could possibly use ONE pop up window for EVERYONE. Whoever's button get clicked, that is the info that populates/shows on the display window.
+// Need a close form function if i go that route to close the window.
